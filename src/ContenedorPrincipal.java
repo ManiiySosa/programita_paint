cb85077bd7d7847409bcaf0d47993590344497c4
Maniiy <raybanss13@gmail.com>
2021-03-08 19:27:27 -0600

    jugando con lineas

--- src/ContenedorPrincipal.java
+++ src/ContenedorPrincipal.java
@@ -1,4 +1,6 @@
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import javafx.scene.control.Alert;
@@ -14,7 +16,11 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Line;
 
 public class ContenedorPrincipal extends BorderPane {
 
@@ -22,14 +28,18 @@ public class ContenedorPrincipal extends BorderPane {
     private Menu menuArchivo, menuEdicion;
     private MenuItem opcionNuevo, opcionSalir, opcionCopiar, opcionPegar, opcionCortar;
     private ToolBar barraHerramientas;
+    private Pane areaDibujo;
 
     public ContenedorPrincipal(){
         crearBarraMenus();
         crearBarraHerramientas();
 
         VBox contenedorSuperior = new VBox();
+        areaDibujo = new Pane();
+        crearFormas(areaDibujo);
         contenedorSuperior.getChildren().addAll(barraMenus, barraHerramientas);
         setTop(contenedorSuperior);
+        setCenter(areaDibujo);
     }
 
     public void crearBarraMenus(){
@@ -68,5 +78,35 @@ public class ContenedorPrincipal extends BorderPane {
         if(result.isPresent() && result.get() == ButtonType.OK){
             System.exit(0);
         }
+    }
+
+    public void crearFormas(Pane areadibujo){
+        List<Line> lineas = new ArrayList<Line>();
+        for(int i=0; i<200; i++){
+            Line linea = new Line(300, 100+i, 300+i, 300);
+            Line linea2 = new Line(300, 100+i, 300-i, 300);
+            Line linea3 = new Line(300, 500-i, 300+i, 300);
+            Line linea4 = new Line(300, 500-i, 300-i, 300);
+
+
+            double r, g, v;
+            r = Math.random()*256;
+            g = Math.random()*256;
+            v = Math.random()*256;
+            Paint color = Color.rgb((int)r, (int)g, (int)v);
+
+            linea.setStroke(color);
+            linea2.setStroke(color);
+            linea3.setStroke(color);
+            linea4.setStroke(color);
+            lineas.add(linea);
+            lineas.add(linea2);
+            lineas.add(linea3);
+            lineas.add(linea4);
+
+        }
+
+        areadibujo.getChildren().addAll(lineas);
+                                     
     }
 }
